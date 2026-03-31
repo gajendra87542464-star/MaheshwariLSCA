1ec2f6d818850a80ef24da68886ae3c0340feb19
Gajendra <gajendra87542464@gmail.com>
2026-03-31 23:08:42 +0530

    frontend file added

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,17 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+
+// FRONTEND FILES SERVE
+app.use(express.static("public"));
+
 app.use("/api/auth", require("./routes/auth.js"));
 
-app.listen(5000, ()=>{
+// PORT FOR RENDER
+const PORT = process.env.PORT || 5000;
 
-console.log("Server running on port 5000");
+app.listen(PORT, ()=>{
+
+console.log(`Server running on port ${PORT}`);
 
 });
